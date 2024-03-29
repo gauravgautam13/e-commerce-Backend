1f4058155b3086c4cae9bc252f7f4ed34602a11c
Gaurav Gautam <149467793+gauravgautam13@users.noreply.github.com>
2024-03-29 16:49:20 +0530

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -1,49 +1,7 @@
-// const product = require("../models/product");
-// const getAllProducts = async (req, res) => {
-//     const { page = 1, limit = 12 , query} = req.query;
-//     const category = req.query.category;
-//     const _id = req.query._id;
-//     // console.log(category);
-//     console.log(_id);
-
-//     console.log(req.query)
-//     const searchQuery = { $or :[
-//         {name : {$regex : query || '', $options: "i"}},
-//         {description : {$regex : query || '', $options: "i"}},
-//        // {category : {$regex : query || '' , $options: "i"}},
-    
-//     ]
-//     }
-
-//      if (category) {
-//     searchQuery.category = category;
-//   }
-//     if (_id) {
-//     searchQuery._id = _id;
-//   }
-
-//     try {
-//         const myData = await product.find(searchQuery) 
-//         .limit(limit * 1)
-//         .skip((page - 1) * limit)
-//         // .exec();
-        
-//         const count = await product.count(searchQuery);
-//         const n_pages = await count / limit;
-//       return res.status(200).json({myData, count, n_pages});
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-
-
-// module.exports = {getAllProducts};
-
-
 const product = require("../models/product");
 
 const getAllProducts = async (req, res) => {
-  const { page = 1, limit = 12, query, _id } = req.query; // Fixed here
+  const { page = 1, limit = 12, query, _id } = req.query; 
   const category = req.query.category;
   
 
